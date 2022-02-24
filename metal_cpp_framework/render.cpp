a861bdf63d459ec80e816477285e124ce446082c
Yuan Li <vanish8.7@gmail.com>
2022-02-24 12:11:44 +0900

    print featruer set info

--- metal_cpp_framework/render.cpp
+++ metal_cpp_framework/render.cpp
@@ -9,10 +9,12 @@ void RenderFunction()
 	auto device = MTL::CreateSystemDefaultDevice();
 	if(device->supportsFeatureSet(MTL::FeatureSet::FeatureSet_macOS_GPUFamily2_v1))
 	{
-		std::cout<<"GPU device create"<<std::endl;
+		std::cout<<"GPU device create "<<"iOS_GPUFamily4_v1"<<std::endl;
 	}
-    //printf("%s", );
+	else if(device->supportsFeatureSet(MTL::FeatureSet::FeatureSet_iOS_GPUFamily4_v1))
+	{
+		std::cout<<"GPU device create "<<"FeatureSet_iOS_GPUFamily4_v1"<<std::endl;
+	}
     std::cout<<device->name()->cString(NS::UTF8StringEncoding)<<std::endl;
-
 	// delete device;
 }
